462d4f0fdbfc7b3516c886dd983c3ddd1d32cc28
woab <wa@9077BG3.MobicaPL.local>
2023-02-09 10:29:47 +0100

    Formatting fixed

--- 01_simple_char_driver/lesson_1/hello.c
+++ 01_simple_char_driver/lesson_1/hello.c
@@ -1,24 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
- 
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("WOAB");
 MODULE_DESCRIPTION("Hello world module");
 MODULE_VERSION("0.1");
- 
+
 static char *name = "default";
 module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name to display");
- 
-static int __init hello_init(void){
+
+static int __init hello_init(void)
+{
 	printk(KERN_INFO "WOAB: Hello %s!\n", name);
 	return 0;
 }
- 
-static void __exit hello_exit(void){
+
+static void __exit hello_exit(void)
+{
 	printk(KERN_INFO "WOAB: Goodbye %s!\n", name);
 }
- 
+
 module_init(hello_init);
 module_exit(hello_exit);
--- 01_simple_char_driver/lesson_2/chardev.c
+++ 01_simple_char_driver/lesson_2/chardev.c
@@ -5,45 +5,46 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 
-#define DEVICE_NAME	"woabchar"
-#define CLASS_NAME	"woab"
+#define DEVICE_NAME "woabchar"
+#define CLASS_NAME "woab"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("woab");
 MODULE_DESCRIPTION("A simple Linux char driver");
 MODULE_VERSION("0.1");
 
-static int	majorNumber;
-static char	message[256] = {0};
-static short	size_of_message;
-static int	numberOpens = 0;
-static struct	class*  ebbcharClass  = NULL;
-static struct	device* ebbcharDevice = NULL;
+static int majorNumber;
+static char message[256] = { 0 };
+static short size_of_message;
+static int numberOpens = 0;
+static struct class *ebbcharClass = NULL;
+static struct device *ebbcharDevice = NULL;
 
-static int	dev_open(struct inode *, struct file *);
-static int	dev_release(struct inode *, struct file *);
-static ssize_t	dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t	dev_write(struct file *, const char *, size_t, loff_t *);
+static int dev_open(struct inode *, struct file *);
+static int dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
-static struct file_operations fops =
-{
+static struct file_operations fops = {
 	.open = dev_open,
 	.read = dev_read,
 	.write = dev_write,
 	.release = dev_release,
 };
 
-
 static int __init woabchar_init(void)
 {
 	printk(KERN_INFO "WOABchar: Initializing the WOABchar LKM\n");
 
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-	if (majorNumber < 0){
-		printk(KERN_ALERT "WOABchar failed to register a major number\n");
+	if (majorNumber < 0) {
+		printk(KERN_ALERT
+		       "WOABchar failed to register a major number\n");
 		return majorNumber;
 	}
-	printk(KERN_INFO "WOABchar: registered correctly with major number %d\n", majorNumber);
+	printk(KERN_INFO
+	       "WOABchar: registered correctly with major number %d\n",
+	       majorNumber);
 
 	ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
 	if (IS_ERR(ebbcharClass)) {
@@ -53,7 +54,8 @@ static int __init woabchar_init(void)
 	}
 	printk(KERN_INFO "WOABchar: device class registered correctly\n");
 
-	ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+	ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0),
+				      NULL, DEVICE_NAME);
 	if (IS_ERR(ebbcharDevice)) {
 		class_destroy(ebbcharClass);
 		unregister_chrdev(majorNumber, DEVICE_NAME);
@@ -75,35 +77,42 @@ static void __exit woabchar_exit(void)
 static int dev_open(struct inode *inodep, struct file *filep)
 {
 	numberOpens++;
-	printk(KERN_INFO "WOABchar: Device has been opened %d time(s)\n", numberOpens);
+	printk(KERN_INFO "WOABchar: Device has been opened %d time(s)\n",
+	       numberOpens);
 	return 0;
 }
 
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
+			loff_t *offset)
 {
 	int error_count = 0;
-	
+
 	error_count = copy_to_user(buffer, message, size_of_message);
 
 	if (0 == error_count) {
-	  printk(KERN_INFO "WOABchar: Sent %d characters to the user\n", size_of_message);
-	  return size_of_message;
-	}
-	else {
-	  printk(KERN_INFO "WOABchar: Failed to send %d characters to the user\n", error_count);
-	  return -EFAULT;
+		printk(KERN_INFO "WOABchar: Sent %d characters to the user\n",
+		       size_of_message);
+		return size_of_message;
+	} else {
+		printk(KERN_INFO
+		       "WOABchar: Failed to send %d characters to the user\n",
+		       error_count);
+		return -EFAULT;
 	}
 }
 
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len,
+			 loff_t *offset)
 {
 	int uncopied;
 	size_of_message = len < sizeof(message) ? len : sizeof(message);
 	uncopied = copy_from_user(message, buffer, size_of_message);
-	printk(KERN_INFO "WOABchar: Received %zu (%lu uncopied) characters from the user"
-			", size_of_message = %d\n", len, 
-			uncopied + (len < sizeof(message) ? 0 : len - sizeof(message)),
-			size_of_message);
+	printk(KERN_INFO
+	       "WOABchar: Received %zu (%lu uncopied) characters from the user"
+	       ", size_of_message = %d\n",
+	       len,
+	       uncopied + (len < sizeof(message) ? 0 : len - sizeof(message)),
+	       size_of_message);
 	return len;
 }
 
