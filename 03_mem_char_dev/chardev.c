7547154719024377a9856dfd97f4c92cf85081fb
woab <wa@9077BG3.MobicaPL.local>
2023-02-10 11:40:36 +0100

    Fixed formatting

--- 03_mem_char_dev/chardev.c
+++ 03_mem_char_dev/chardev.c
@@ -4,7 +4,7 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-#include <linux/slab.h>	
+#include <linux/slab.h>
 #include <linux/cdev.h>
 
 #define DEVICE_NAME "woabchar"
@@ -52,32 +52,33 @@ static struct woab_dev *woab_device;
 
 static int woab_device_alloc_buffer(size_t buffer_length)
 {
-	if(!woab_device)
+	if (!woab_device)
 		return -ENXIO;
 	kfree(woab_device->data);
-	woab_device->data = (char*)kmalloc(buffer_length * sizeof(char), GFP_KERNEL);
+	woab_device->data =
+		(char *)kmalloc(buffer_length * sizeof(char), GFP_KERNEL);
 	if (!woab_device->data) {
 		printk(KERN_WARNING "WOABchar: ERROR kmalloc data\n");
 		return -ENOMEM;
 	}
 	woab_device->buffer_length = buffer_length;
-	
+
 	return 0;
 }
 
 static int woab_device_init(void)
 {
 	int ret = 0;
-	
+
 	woab_device = kmalloc(sizeof(struct woab_dev), GFP_KERNEL);
 	if (!woab_device) {
 		printk(KERN_WARNING "WOABchar: ERROR kmalloc dev struct\n");
-		return -ENOMEM; 
+		return -ENOMEM;
 	}
 	memset(woab_device, 0, sizeof(struct woab_dev));
-	
+
 	printk(KERN_INFO "WOABchar: Initializing the WOABchar LKM\n");
-	
+
 	return ret;
 }
 
@@ -95,7 +96,7 @@ static void woab_setup_cdev(struct woab_dev *dev)
 	cdev_init(&dev->cdev, &woab_fops);
 	dev->cdev.owner = THIS_MODULE;
 	dev->cdev.ops = &woab_fops;
-	err = cdev_add (&dev->cdev, devno, 1);
+	err = cdev_add(&dev->cdev, devno, 1);
 	if (err)
 		printk(KERN_WARNING "[WOABchar] Error %d adding cdev", err);
 
@@ -106,25 +107,25 @@ static int __init woabchar_init(void)
 {
 	int ret;
 	dev_t dev = 0;
-	
-	ret = alloc_chrdev_region(&dev, woabchar_minor, 
-				woabchar_nr_devices, "woabchar");
+
+	ret = alloc_chrdev_region(&dev, woabchar_minor, woabchar_nr_devices,
+				  "woabchar");
 	woabchar_major = MAJOR(dev);
 
 	if (ret < 0) {
 		printk(KERN_WARNING "[WOABchar]: Can't get major %d\n",
-				woabchar_major);
+		       woabchar_major);
 		return ret;
 	}
 
 	ret = woab_device_init();
-	if(ret)
+	if (ret)
 		return ret;
 
 	woab_setup_cdev(woab_device);
-	
+
 	ret = woab_device_alloc_buffer(DEFAULT_BUFFER);
-	if(ret)
+	if (ret)
 		return ret;
 
 	printk(KERN_INFO "[WOABchar]: Device initialized\n");
@@ -133,11 +134,11 @@ static int __init woabchar_init(void)
 }
 
 static void __exit woabchar_exit(void)
-{	
+{
 	dev_t devno = MKDEV(woabchar_major, woabchar_minor);
 	cdev_del(&(woab_device->cdev));
 	unregister_chrdev_region(devno, woabchar_nr_devices);
-	
+
 	woab_device_cleanup();
 	printk(KERN_INFO "WOABchar: Goodbye from the LKM!\n");
 }
@@ -145,24 +146,24 @@ static void __exit woabchar_exit(void)
 static int woab_dev_open(struct inode *inodep, struct file *filep)
 {
 	struct woab_dev *dev;
-	
+
 	dev = container_of(inodep->i_cdev, struct woab_dev, cdev);
 	filep->private_data = dev;
-	
+
 	woab_device->numberOpens++;
-	
+
 	printk(KERN_INFO "WOABchar: Device has been opened %d time(s)\n",
-	       woab_device->numberOpens );
+	       woab_device->numberOpens);
 	return 0;
 }
 
 static ssize_t woab_dev_read(struct file *filep, char *buffer, size_t len,
-			loff_t *offset)
+			     loff_t *offset)
 {
 	int error_count = 0;
 
-	error_count = copy_to_user(buffer, woab_device->data, 
-				woab_device->data_length);
+	error_count = copy_to_user(buffer, woab_device->data,
+				   woab_device->data_length);
 
 	if (0 == error_count) {
 		printk(KERN_INFO "WOABchar: Sent %lu characters to the user\n",
@@ -176,24 +177,24 @@ static ssize_t woab_dev_read(struct file *filep, char *buffer, size_t len,
 	}
 }
 
-static ssize_t woab_dev_write(struct file *filep, const char *buffer, size_t len,
-			 loff_t *offset)
+static ssize_t woab_dev_write(struct file *filep, const char *buffer,
+			      size_t len, loff_t *offset)
 {
 	int uncopied, ret;
-	if(len > woab_device->buffer_length) {
+	if (len > woab_device->buffer_length) {
 		ret = woab_device_alloc_buffer(len);
-		if(ret)
+		if (ret)
 			return ret;
 		printk(KERN_WARNING "WOABchar: New memory alocated succesfully"
-			"with length of %lu\n", len);
+				    " with length of %lu\n",
+		       len);
 	}
-	
+
 	woab_device->data_length = len;
 	uncopied = copy_from_user(woab_device->data, buffer, len);
 	printk(KERN_INFO
 	       "WOABchar: Received %lu (%d uncopied) characters from the user",
-	       len,
-	       uncopied);
+	       len, uncopied);
 	return len;
 }
 
