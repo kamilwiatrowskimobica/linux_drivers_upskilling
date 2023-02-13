672fd289811599edcf7d244d1119484ba2846aca
Mateusz Leszczynski <mateusz.leszczynski@mobica.com>
2023-02-13 11:55:41 +0100

    Modified Makefile, formatting files
    Signed-off-by: Mateusz Leszczynski <mateusz.leszczynski@mobica.com>

--- 03_mem_char_dev/hello.c
+++ 03_mem_char_dev/hello.c
@@ -1,154 +1,234 @@
-#include <linux/init.h>
+/*
+ *
+ * Author  :   Leonardo Suriano <leonardo.suriano@live.it>
+ * Date    :   Dicember 2017
+ *
+ *
+ *
+ *
+ * */
+
+#include <linux/init.h> /* needed for module_init and exit */
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h> // Required for the copy to user function
-#include <linux/device.h> //device_create
-//#include <linux/device/class.h> //class_create
+#include <linux/moduleparam.h> /* needed for module_param */
+#include <linux/kernel.h> /* needed for printk */
+#include <linux/sched.h> /* needed for current-> */
+#include <linux/types.h> /* needed for dev_t type */
+#include <linux/kdev_t.h> /* needed for macros MAJOR, MINOR, MKDEV... */
+#include <linux/fs.h> /* needed for register_chrdev_region, file_operations */
+#include <linux/cdev.h> /* cdev definition */
+#include <linux/slab.h> /* kmalloc(),kfree() */
+#include <asm/uaccess.h> /* copy_to copy_from _user */
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Mateusz Leszczynski, Mobica Ltd.");
-MODULE_DESCRIPTION("A super-simple LKM.");
-MODULE_VERSION("0.1");
+#include "hello.h"
 
-#define DEVICE_NAME "hellodrvchar" /* device at /dev/hellodrvchar */
-#define CLASS_NAME "hellodrv"
-#define MESSAGE_LEN 256
+int hello_major = 0;
+int hello_minor = 0;
+unsigned int hello_nr_devs = 1;
+int device_max_size = DEVICE_MAX_SIZE;
 
-static int majorNumber;
-static char message[MESSAGE_LEN] = {
-	0
-}; ///< string that is passed from userspace
-static short size_of_message; ///< size of the string stored
-static int numberOpens = 0; ///< Counts the number of times the device is opened
-static struct class *hellodrvcharClass = NULL;
-static struct device *hellodrvcharDevice = NULL;
+module_param(hello_major, int, S_IRUGO);
+module_param(hello_minor, int, S_IRUGO);
+//module_param(hello_nr_devs, int, S_IRUGO);
+module_param(device_max_size, int, S_IRUGO);
 
-static int dev_open(struct inode *, struct file *);
-static int dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+struct hello_dev *hello_devices; /* allocated in hello_init_module */
 
-static struct file_operations fops = {
-	.open = dev_open,
-	.read = dev_read,
-	.write = dev_write,
-	.release = dev_release,
+/*
+ * Open and close
+ */
+
+int hello_open(struct inode *inode, struct file *filp)
+{
+	struct hello_dev *dev; /* device information */
+	printk(KERN_INFO "[LEO] performing 'open' operation\n");
+	dev = container_of(inode->i_cdev, struct hello_dev, cdev);
+	filp->private_data = dev; /* for other methods */
+
+	return 0; /* success */
+}
+
+int hello_release(struct inode *inode, struct file *filp)
+{
+	printk(KERN_INFO "[LEO] performing 'release' operation\n");
+	return 0;
+}
+
+/*
+ * The cleanup function is used to handle initialization failures as well.
+ * Thefore, it must be careful to work correctly even if some of the items
+ * have not been initialized
+ */
+
+void hello_cleanup_module(void)
+{
+	dev_t devno = MKDEV(hello_major, hello_minor);
+	cdev_del(&(hello_devices->cdev));
+	/* freeing the memory */
+	if ((hello_devices->p_data) != 0) {
+		kfree(hello_devices->p_data);
+		printk(KERN_INFO "[LEO] kfree the string-memory\n");
+	}
+	if ((hello_devices) != 0) {
+		kfree(hello_devices);
+		printk(KERN_INFO "[LEO] kfree hello_devices\n");
+	}
+	unregister_chrdev_region(devno,
+				 hello_nr_devs); /* unregistering device */
+	printk(KERN_INFO "[LEO] cdev deleted, kfree, chdev unregistered\n");
+}
+
+/*
+ * Data management: read and write
+ */
+
+ssize_t hello_read(struct file *filp, char __user *buf, size_t count,
+		   loff_t *f_pos)
+{
+	//struct hello_dev *dev = filp->private_data;
+	ssize_t retval = 0;
+	if (count > device_max_size) {
+		printk(KERN_WARNING
+		       "[LEO] hello: trying to read more than possible. Aborting read\n");
+		retval = -EFBIG;
+		goto out;
+	}
+	if (copy_to_user(buf, (void *)hello_devices->p_data, count)) {
+		printk(KERN_WARNING "[LEO] hello: can't use copy_to_user. \n");
+		retval = -EPERM;
+		goto out;
+	}
+	printk(KERN_INFO "[LEO] performed READ Operation sucessfully\n");
+	return retval;
+
+out:
+	return retval;
+}
+
+ssize_t hello_write(struct file *filp, const char __user *buf, size_t count,
+		    loff_t *f_pos)
+{
+	int retval = 0;
+	struct hello_dev *dev = filp->private_data;
+	if (count > device_max_size) {
+		printk(KERN_WARNING
+		       "[LEO] hello: trying to write more than possible. Aborting write\n");
+		retval = -EFBIG;
+		goto out;
+	}
+	if (copy_from_user((void *)hello_devices->p_data, buf, count)) {
+		printk(KERN_WARNING
+		       "[LEO] hello: can't use copy_from_user. \n");
+		retval = -EPERM;
+		goto out;
+	}
+	return retval;
+
+out:
+	return retval;
+}
+
+/*
+ * Create a set of file operations for our hello files.
+ * All the functions do nothig
+ */
+
+struct file_operations hello_fops = {
+	.owner = THIS_MODULE,
+	.read = hello_read,
+	.write = hello_write,
+	.open = hello_open,
+	.release = hello_release,
 };
+
+/*
+ * Set up the char_dev structure for this device.
+ */
+static void hello_setup_cdev(struct hello_dev *dev)
+{
+	int err, devno = MKDEV(hello_major, hello_minor);
+
+	cdev_init(&dev->cdev, &hello_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &hello_fops;
+	err = cdev_add(&dev->cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_WARNING "[LEO] Error %d adding hello cdev_add",
+		       err);
+
+	printk(KERN_INFO "[LEO] cdev initialized\n");
+}
+
+/*
+ * The init function is used to register the chdeiv allocating dinamically a
+ * new major number (if not specified at load/compilation-time)
+ */
 
 static int hello_init(void)
 {
-	long rc = 0;
+	int result = 0;
+	dev_t dev = 0;
 
-	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-	if (majorNumber < 0) {
-		printk(KERN_ALERT "Failed to register a major number\n");
-		return majorNumber;
-	}
-
-	// Register the device class
-	hellodrvcharClass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(hellodrvcharClass)) {
-		printk(KERN_ALERT "Failed to register device class\n");
-		rc = PTR_ERR(
-			hellodrvcharClass); // Correct way to return an error on a pointer
-		goto err_class;
-	}
-
-	// Register the device driver
-	hellodrvcharDevice = device_create(hellodrvcharClass, NULL,
-					   MKDEV(majorNumber, 0), NULL,
-					   DEVICE_NAME);
-	if (IS_ERR(hellodrvcharDevice)) { // Clean up if there is an error
-		printk(KERN_ALERT "Failed to create the device\n");
-		rc = PTR_ERR(hellodrvcharDevice);
-		goto err_dev;
-	}
-	printk(KERN_INFO "%s: device class created correctly\n",
-	       DEVICE_NAME); // Made it! device was initialized
-
-	printk(KERN_INFO "MODULE LOADED\n");
-	return rc;
-
-err_dev:
-	device_destroy(hellodrvcharClass, MKDEV(majorNumber, 0));
-err_class:
-	class_destroy(hellodrvcharClass);
-	//TODO is unregister_chrdev() unnecessary?
-	unregister_chrdev(majorNumber, DEVICE_NAME);
-	return rc;
-}
-
-static int dev_open(struct inode *inodep, struct file *filep)
-{
-	numberOpens++;
-	printk(KERN_INFO "%s: Device has been opened %d time(s)\n", DEVICE_NAME,
-	       numberOpens);
-	return 0;
-}
-
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
-			loff_t *offset)
-{
-	int error_count = 0;
-	// copy_to_user has the format ( * to, *from, size) and returns 0 on success
-	error_count = copy_to_user(buffer, message, size_of_message);
-
-	if (error_count == 0) {
-		printk(KERN_INFO "%s: Sent %d characters to the user\n",
-		       DEVICE_NAME, size_of_message);
-		return (size_of_message = 0);
+	if (hello_major) {
+		printk(KERN_INFO
+		       "[LEO] static allocation of major number (%d)\n",
+		       hello_major);
+		dev = MKDEV(hello_major, hello_minor);
+		result = register_chrdev_region(dev, hello_nr_devs, "hello");
 	} else {
-		printk(KERN_INFO
-		       "%s: Failed to send %d characters to the user\n",
-		       DEVICE_NAME, error_count);
-		return -EFAULT;
+		printk(KERN_INFO "[LEO] dinamic allocation of major number\n");
+		result = alloc_chrdev_region(&dev, hello_minor, hello_nr_devs,
+					     "hello");
+		hello_major = MAJOR(dev);
 	}
-}
-
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len,
-			 loff_t *offset)
-{
-	long left;
-
-	// sanity check
-	if (len >= MESSAGE_LEN) {
-		printk(KERN_INFO
-		       "%s: too long message from the user. Should be less than %u chars\n",
-		       DEVICE_NAME, MESSAGE_LEN);
+	if (result < 0) {
+		printk(KERN_WARNING "[LEO] hello: can't get major %d\n",
+		       hello_major);
+		return result;
 	}
 
-	left = copy_from_user(message, buffer, len);
-	if (left) {
-		printk(KERN_INFO
-		       "%s: problem on receiving characters from the user\n",
-		       DEVICE_NAME);
+	hello_devices =
+		kmalloc(hello_nr_devs * sizeof(struct hello_dev), GFP_KERNEL);
+	if (!hello_devices) {
+		result = -ENOMEM;
+		printk(KERN_WARNING "[LEO] ERROR kmalloc dev struct\n");
+		goto fail; /* Make this more graceful */
 	}
 
-	size_of_message = strnlen(message, MESSAGE_LEN);
-	printk(KERN_INFO "%s: Received %zu characters from the user\n",
-	       DEVICE_NAME, len);
-	printk(KERN_INFO "%s: Received message: %s\n", DEVICE_NAME, message);
-	return len;
-}
+	memset(hello_devices, 0, hello_nr_devs * sizeof(struct hello_dev));
+	/* Initialize the device. */
 
-static int dev_release(struct inode *inodep, struct file *filep)
-{
-	printk(KERN_INFO "%s: Device successfully closed\n", DEVICE_NAME);
+	hello_devices->p_data =
+		(char *)kmalloc(device_max_size * sizeof(char), GFP_KERNEL);
+	if (!hello_devices->p_data) {
+		result = -ENOMEM;
+		printk(KERN_WARNING "[LEO] ERROR kmalloc p_data\n");
+		goto fail; /* Make this more graceful */
+	}
+	hello_setup_cdev(hello_devices);
+
 	return 0;
+
+fail:
+	hello_cleanup_module();
+	return result;
 }
+
+/*
+ * The exit function is simply calls the cleanup
+ */
 
 static void hello_exit(void)
 {
-	device_destroy(hellodrvcharClass,
-		       MKDEV(majorNumber, 0)); // remove the device
-	class_unregister(hellodrvcharClass); // unregister the device class
-	class_destroy(hellodrvcharClass); // remove the device class
-	//TODO is unregister_chrdev() unnecessary?
-	unregister_chrdev(majorNumber,
-			  DEVICE_NAME); // unregister the major number
-	printk(KERN_INFO "MODULE UNLOADED\n");
+	printk(KERN_INFO "[LEO] --calling cleanup function--\n");
+	hello_cleanup_module();
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Leonardo Suriano <leonardo.suriano@live.it>");
+MODULE_DESCRIPTION("example of using read");
+MODULE_VERSION("1.0");
