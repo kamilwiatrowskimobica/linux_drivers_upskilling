5344855f256c098b65f68020ea8cb9b817acb1dc
Mateusz Leszczynski <mateusz.leszczynski@mobica.com>
2023-02-28 16:05:41 +0100

    implement sequence file in /proc
    The file is /proc/mtle_sequence
    cat'ting on a file causes printing sequence of numbers.
    This is based on exercise 03_ and example by J. Corbet and the
    https://www.kernel.org/doc/html/next/filesystems/seq_file.html page
    (date of acccess: 2023-02-28 16:08)
    
    Reducing time to wait msleep_interruptible() might be desirable.
    
    Signed-off-by: Mateusz Leszczynski <mateusz.leszczynski@mobica.com>

---
+++ 04_char_dev_proc/load_module.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# This is to load module and test whether it is currently loaded
+
+set -e
+
+module_name="hello"
+device="hellodrvchar"
+mode="664"
+
+
+#printf "Help:\n\t$0 [options_to_load_module]\nRun with su priviledges\n\n"
+
+if [[ ${module_name} =~ ".ko" ]]; then
+        module_file="${module_name}"
+else
+        module_file="${module_name}.ko"
+fi
+PARAM_DIR=" /sys/module/${module_name}/parameters"
+
+#WHO="$(whoami)"
+#if [[ $WHO != "root" ]]; then
+#        printf "run this script with su priviledges!\n"
+#        exit
+#fi
+
+ADDRESS=$(sudo grep hello /proc/modules |awk '{print $6}')
+objdump -dS --adjust-vma=$(ADDRESS) hello.ko
+
+modinfo ${module_file}
+
+
+#reload module
+if [[ $(lsmod|grep -c ${module_name} ) == 1 ]] ; then
+	rmmod ${module_name}
+fi
+insmod ${module_file} $* || exit 1
+
+#test if module is loaded
+if [[ $(lsmod|grep -c ${module_name} ) == 1 ]] ; then
+        printf "SUCCESS\n"
+else
+        printf "FAIL - no module of that name\n"
+fi
+
+
+echo "/PROC/DEVICES allows retrieving major number of a device"
+cat /proc/devices|grep ${device}
+# retrieve major number (returns multiple, if more than one)
+major=$(awk "\$2==\"${device}\" {print \$1}" /proc/devices)
+# retrieve major number (returns random, not necessarily the last assigned)
+#major=$(awk "\$2==\"${device}\" {print \$1; exit}" /proc/devices)
+#rm -f /dev/${device}[0-3]
+# create a char device with og+rw permissions
+mknod -m og+rw /dev/${device}0 c ${major} 0
+##mknod /dev/${device}0 c ${major} 0
+##mknod /dev/${device}1 c ${major} 1
+##mknod /dev/${device}2 c ${major} 2
+##mknod /dev/${device}3 c ${major} 3
+ln -sf /dev/${device}0 /dev/${device}
+##chgrp $group /dev/${device}[0-3]
+##chmod $mode  /dev/${device}[0-3]
+
+echo "/PROC/MODULES"
+cat /proc/modules | grep ${module_name}
+
+echo CHECKING ${PARAM_DIR} if module does accept any params
+if [[ -e ${PARAM_DIR} ]]; then
+        ls -l ${PARAM_DIR}
+else
+        printf "${PARAM_DIR} does not exist\n"
+fi
+
+#echo "UDEVADM INFO - usefull for creating /etc/udev/rules.d"
+#udevadm info -a -p /sys/class/hellodrv/hellodrvchar
+#
+#if [[ -e /etc/udev/rules.d/99-hellodrv.rules ]]; then
+#        echo "UDEV RULES EXIST"
+#else
+#        echo "CREATING UDEV RULES"
+#        touch /etc/udev/rules.d/99-hellodrv.rules
+#        echo "KERNEL==\"hellodrvchar\", SUBSYSTEM==\"hellodrv\", MODE=\"0666\"" >> /etc/udev/rules.d/99-hellodrv.rules
+#fi
+
+echo "STRACE TEST"
+strace ./test
+
+#rmmod ${module_file}
+
+
+
+
