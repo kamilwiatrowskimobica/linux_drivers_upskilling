062d442fbeca3b13f0637dada5e93d6edbf4ae3e
woab <wa@9077BG3.MobicaPL.local>
2023-03-09 06:16:29 +0100

    Fixed IRQ

--- 11_linked_list/src/woab_irq.c
+++ 11_linked_list/src/woab_irq.c
@@ -14,9 +14,11 @@ spinlock_t status_spinlock;
 static irqreturn_t woab_irq_handler(int irq, void *dev_id)
 {
 	int status_cpy, i;
-	spin_lock(&status_spinlock);
+	unsigned long flags;
+
+	spin_lock_irqsave(&status_spinlock, flags);
 	status_cpy = status_registers;
-	spin_unlock(&status_spinlock);
+	spin_unlock_irqrestore(&status_spinlock, flags);
 	if (0 == status_registers) {
 		WINFO("Interrupt triggered by other device\n");
 		return IRQ_NONE;
@@ -29,9 +31,9 @@ static irqreturn_t woab_irq_handler(int irq, void *dev_id)
 	}
 
 	// clear bits
-	spin_lock(&status_spinlock);
+	spin_lock_irqsave(&status_spinlock, flags);
 	status_registers = status_cpy;
-	spin_unlock(&status_spinlock);
+	spin_unlock_irqrestore(&status_spinlock, flags);
 
 	tasklet_schedule(tasklet);
 	return IRQ_HANDLED;
@@ -69,14 +71,16 @@ void __exit woab_irq_close(void)
 int woab_irq_trigger(int dev_idx)
 {
 	struct irq_desc *desc = NULL;
+	unsigned long flags;
+
 	desc = irq_data_to_desc(irq_get_irq_data(11));
 	if (!desc) {
 		return -EINVAL;
 	}
 
-	spin_lock(&status_spinlock);
+	spin_lock_irqsave(&status_spinlock, flags);
 	status_registers |= (1 << dev_idx);
-	spin_unlock(&status_spinlock);
+	spin_unlock_irqrestore(&status_spinlock, flags);
 
 	__this_cpu_write(vector_irq[59], desc);
 	asm("int $0x3B"); // Corresponding to irq 11
