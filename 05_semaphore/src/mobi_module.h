e47274e0e3e441916fd29c2f4ed1994aeccf8e65
Lukasz Krakowiak <lukasz.krakowiak@mobica.com>
2023-03-17 17:05:44 +0100

    add 05_semaphore

---
+++ 05_semaphore/src/mobi_module.h
@@ -0,0 +1,16 @@
+#ifndef _MOBI_MODULE_H_
+#define _MOBI_MODULE_H_
+
+#include <linux/cdev.h>
+#include <linux/mutex.h>
+
+struct mob_dev {
+    size_t id;
+    size_t msg_size;
+    struct mutex lock;
+    struct cdev cdev;
+    char *msg;
+};
+
+
+#endif /* _MOBI_MODULE_H_ */
