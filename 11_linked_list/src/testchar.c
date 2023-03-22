9e661ff4d8e6ffb943fba3c4f7ed8b3879b72a6b
AKPetkovic <andrej.petkovic@ext.mobica.com>
2023-03-22 10:51:27 +0100

    test programs

---
+++ 11_linked_list/src/testchar.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFF_LEN (256u)
+static char recv[BUFF_LEN];
+
+int main(int argsc, char **argsv)
+{
+	int ret, fd;
+	char send[BUFF_LEN];
+	char device[20] = "/dev/mobchar";
+
+	printf("Starting the device test...\n");
+
+	strncat(device, argsv[1], strlen(device));
+
+	fd = open(device, O_RDWR);
+
+	// open err check
+	if (fd < 0) {
+		perror("Failed to open the device...");
+		return errno;
+	}
+
+	printf("Type in a string to send to KM:\n");
+	// read until \n
+	scanf("%[^\n]%*c", send);
+	printf("Input string for device %s\n", send);
+
+	// send string
+	ret = write(fd, send, strlen(send));
+	if (ret < 0) {
+		perror("Failed to write string to dev...");
+		return errno;
+	}
+
+	printf("Press ENTER to read back from dev\n");
+	// hold char
+	getchar();
+
+	printf("Reading from device...\n");
+	ret = read(fd, recv, BUFF_LEN);
+	if (ret < 0) {
+		perror("Failed to read the msg from dev...");
+		return errno;
+	}
+
+	printf("Received msg: %s\n", recv);
+	printf("END\n");
+
+	return 0;
+}
