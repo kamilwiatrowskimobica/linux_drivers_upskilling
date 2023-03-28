8cab073450a04e873ba8c752f27e03baee902598
Piotr Wozniak <piotr.wozniak@mobica.com>
2023-03-28 10:36:38 +0200

    05 wait for complete
    Signed-off-by: Piotr Wozniak <piotr.wozniak@mobica.com>

---
+++ 05_complete/pwmobchar.h
@@ -0,0 +1,13 @@
+#ifndef _PWMOB_H_
+#define _PWMOB_H_
+
+#define PWMOB_DATA_MAX 100
+
+struct pwmob_dev {
+	char *p_data; /* pointer to the memory allocated */
+	size_t data_size;
+	struct cdev cdev; /* Char device structure		*/
+	struct completion sync;
+};
+
+#endif /* _PWMOB_H_ */
